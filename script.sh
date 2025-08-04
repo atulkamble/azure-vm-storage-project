f9f8edd4ca534c3c9c5e8d08153aa0174c23d784
Atul Kamble <atul_kamble@hotmail.com>
2025-08-04 20:48:26 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,18 @@
-#$bin/bash
+#!/bin/bash
 sudo apt update -y
 sudo apt install apache2 -y
-sudo systectl start apache2
+sudo systemctl start apache2
 sudo systemctl enable apache2
 cd /var/www/html 
 sudo chmod 755 /var/www/html
-sudo touch index.html 
-echo "hello world" > index.html 
+sudo tee index.html > /dev/null <<EOF
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Hello World</h1>
+</body>
+</html>
+EOF
